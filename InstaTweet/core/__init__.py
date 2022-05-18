131f5895ddd166e9a0dd237fc0523e5c06112ed7
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-05-18 02:13:30 -0400

    Delete profile.py

--- InstaTweet/core/__init__.py
+++ InstaTweet/core/__init__.py
@@ -3,4 +3,3 @@ from .instauser import InstaUser
 from .instaclient import InstaClient
 from .tweetclient import TweetClient
 from .instatweet import InstaTweet
-from .profile import LocalProfile, DBProfile
