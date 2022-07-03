24747eab84f8316d9a29e86a3bc9eb5c4ef6a0de
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-07-02 22:25:53 -0400

    Add `get_agents()` to remove bs4 as dependency
    * Taken from my other repo: https://github.com/TDKorn/my-magento/blob/main/magento/utils.py#L339
    
    * It's uglier but it removes the bs4 requirement, although I never added it anyways??
        -  Literally what was I doing lol, no way I was this bad in January...

--- InstaTweet/utils.py
+++ InstaTweet/utils.py
@@ -1,19 +1,28 @@
 import os
 import requests
+
 from pathlib import Path
-from bs4 import BeautifulSoup as bs
-
-BACKUP_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36'
 
 
-def get_agent():
-    r = requests.get('https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome')
-    if r.ok:
-        soup = bs(r.text, 'html.parser')
-        if soup_agents := soup.find_all('span', {'class': 'code'}):
-            return soup_agents[0].text
-    # If function fails to scrape, will use hardcoded user agent
-    return BACKUP_AGENT
+AGENTS = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36']
+
+
+def get_agents() -> list:
+    """Scrapes a list of user agents. Returns a default list if the scrape fails."""
+    if (response := requests.get('https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome')).ok:
+        section = response.text.split('<h2>Latest Chrome on Windows 10 User Agents</h2>')[1]
+        raw_agents = section.split('code\">')[1:]
+        agents = [agent.split('<')[0] for agent in raw_agents]
+        for a in agents:
+            if a not in AGENTS:
+                AGENTS.append(a)
+    # If function fails, will still return the hardcoded list
+    return AGENTS
+
+
+def get_agent(index=0) -> str:
+    """Returns a single user agent string from the specified index of the AGENTS list"""
+    return get_agents()[index]  # Specify index only if you hardcode more than 1
 
 
 def get_root():
