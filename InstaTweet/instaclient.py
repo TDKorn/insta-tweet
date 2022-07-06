a369b03e4a396cb216cee7ef16304d85103c843f
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-07-05 21:33:30 -0400

    Update `InstaClient` (Endpoint was deprecated)
    * Replace the ?__a=1 endpoint with one that isn't deprecated
        - This is definitely a temporary fix
        - I want to use someone else's package bc mine is too minimalistic but I literally can't get anything to work so...
    
    * Also changed it to make use of the new Profile class

--- InstaTweet/instaclient.py
+++ InstaTweet/instaclient.py
@@ -1,77 +1,72 @@
 import requests
-from . import InstaPost, InstaUser
+from typing import Union
+from json.decoder import JSONDecodeError
+
+from . import Profile, InstaUser, InstaPost
 from .utils import get_filepath
 
 
-class InstaClient(object):
-    """
-    Instagram client to scrape and download posts
-    """
+class InstaClient:
 
-    def __init__(self, profile: dict):
-        self.session_id = profile['session_id']
-        self.user_map = profile['user_map']
-        self.user_agent = profile['user_agent']
+    def __init__(self, profile: Profile):
+        if not profile.session_id:
+            raise ValueError(f'Profile is missing Instagram sessionid cookie')
+        else:
+            self.profile = profile
 
-    def request(self, url):
+    def request(self, url: str) -> requests.Response:
         return requests.get(url, headers=self.headers, cookies=self.cookies)
 
-    def check_posts(self, username, amount=12):
-        print('Checking posts for @' + username)
+    def get_user(self, username: str) -> InstaUser:
+        response = self.request(f'https://www.instagram.com/{username}/?__a=1&__d=dis')
+        try:
+            return InstaUser(response.json())
+        # Response status code seems to always be 200, but JSON data is only available if actually successful
+        except JSONDecodeError as j:
+            raise DeprecationWarning(
+                f'Unable to scrape Instagram user @{username}. Endpoint has likely been deprecated') from j
+        # In case I'm wrong and there's other reasons for failed requests...
+        except Exception as e:
+            raise RuntimeError(f'Unable to scrape Instagram user @{username}') from e
+
+    def check_posts(self, username, amount=12) -> Union[list[InstaPost], None]:
+        print(f'Checking posts for @{username}')
+        scraped_posts = self.profile.user_map[username]['scraped']
         user = self.get_user(username)
-        scraped_posts = self.user_map[username]['scraped']
 
         if scraped_posts:
-            # Return a list of {amount} InstaPost objects, sorted from oldest to newest (oldest is tweeted first).
-            posts = [post for post in user.posts if post.id not in scraped_posts][:amount]
-            return sorted(posts, key=lambda p: p.timestamp)
+            new_posts = [post for post in user.posts if post.id not in scraped_posts][:amount]
+            return sorted(new_posts, key=lambda post: post.timestamp)
         else:
-            # By default, newly added users have an empty scraped post list and are only initialized on the first run.
-            scraped_posts.extend([post.id for post in user.posts])
+            scraped_posts.extend(post.id for post in user.posts)
             print(f'Initialized User: @{username}')
-            return None     # No posts to tweet
-
-    def get_user(self, username):
-        response = self.request(f'https://www.instagram.com/{username}/?__a=1')
-        if not response.ok:
-            raise Exception(response.json())
-        else:
-            return InstaUser(response.json())
 
     def download_post(self, post: InstaPost, filepath=None):
         response = self.request(post.media_url)
         if not response.ok:
-            raise RuntimeError(f'Failed to download post {post.permalink} by {post.owner["username"]}')
-
+            raise RuntimeError(
+                f'Failed to download post {post.permalink} by {post.owner["username"]}'
+            )
         if filepath is None:
-            filetype = 'mp4' if post.is_video else 'jpg'
-            filepath = get_filepath(post.id, filetype=filetype)
+            filepath = get_filepath(
+                filename=post.id,
+                filetype='mp4' if post.is_video else 'jpg'
+            )
         with open(filepath, 'wb') as f:
             f.write(response.content)
+            post.filepath = filepath
 
-        post.file_path = filepath
-        print(f'Downloaded post {post.id} by {post.owner["username"]} from {post.permalink}')
+        print(f'Downloaded post {post.permalink} by {post.owner["username"]} to {post.filepath}')
+        return True
 
-    # Properties so that changes to sessionid/useragent will be reflected
-    @property
-    def cookies(self):
-        return {
-            'sessionid': self.session_id
-        }
-
-    # TODO See if any other cookies/headers should be included
     @property
     def headers(self):
         return {
-            'User-Agent': self.user_agent
+            'User-Agent': self.profile.user_agent
         }
 
     @property
-    def session_id(self):
-        return self._session_id
-
-    @session_id.setter
-    def session_id(self, session_id: str):
-        if not isinstance(session_id, str):
-            raise ValueError('Session ID cookie must be of type str')
-        self._session_id = session_id
+    def cookies(self):
+        return {
+            'sessionid': self.profile.session_id
+        }
