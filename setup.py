c5bd71970ef967b7b2aefc1b2e63b70ac176ebe2
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-07-09 06:42:01 -0400

    Create setup.py for 2.0.0-beta
    im impatient

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+
+def get_description():
+    file = os.path.abspath('README.md')
+    with open(file, 'r', encoding='utf-8') as f:
+        long_description = u'{}'.format(f.read())
+        return long_description
+
+
+long_description = get_description()
+
+
+setup(
+    name='insta-tweet',
+    packages=['InstaTweet'],
+    version='2.0.0-beta',
+    license='MIT',
+    description='Automatically Repost Content From Instagram to Twitter',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Adam Korn',
+    author_email='hello@dailykitten.net',
+    url='https://www.github.com/TDKorn/insta-tweet',
+    download_url="https://github.com/TDKorn/insta-tweet/tarball/master",
+    keywords=['instagram', 'twitter', 'repost', 'reposter', 'instascrape', 'instagram-repost'],
+    install_requires=["requests"]
+
+)
