2e86bcf9ff1d262f9ac43ca54689f338de32bee3
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-07-15 14:21:19 -0400

    Update InstaPost/InstaUser

--- InstaTweet/instapost.py
+++ InstaTweet/instapost.py
@@ -4,10 +4,11 @@ from datetime import datetime
 
 class InstaPost:
 
-    def __init__(self, post_data):
-        """Convenience wrapper for Instagram media data"""
-        self.json = post_data
-        self.id = post_data['id']  # Something's wrong if this raises an error
+    def __init__(self, data):
+        """Minimalistic API response wrapper for an Instagram post"""
+        self.json = data
+        self.id = data['id']  # Something's wrong if this raises an error
+
         self.is_video = self.json.get('is_video', False)
         self.video_url = self.json.get('video_url', '')
         self.dimensions = self.json.get('dimensions', {})
@@ -18,15 +19,19 @@ class InstaPost:
     def __str__(self):
         return f'Post {self.id} by @{self.owner["username"]} on {self.timestamp}'
 
+    def add_tweet_data(self, tweet: "tweepy.models.Status"):
+        self.tweet_data = {
+            'link': tweet.entities['urls'][0]['url'],
+            'created_at': str(tweet.created_at),
+            'text': tweet.text
+        }
+        return True
+
     @property
     def owner(self):
         if owner := self.json.get('owner', self.json.get('user', {})):
             return owner
         return dict.fromkeys(['id', 'username'])
-
-    @property
-    def is_carousel(self):
-        return self.json.get('media_type') == 8
 
     @property
     def shortcode(self):
@@ -37,21 +42,10 @@ class InstaPost:
         return f'https://www.instagram.com/p/{self.shortcode}'
 
     @property
-    def thumbnail_url(self):
-        return self.json.get('display_url',
-                             self.json.get('thumbnail_src',
-                                           self.json.get('thumbnail_resources',
-                                                         [{}])[-1].get('src', '')))
-
-    @property
     def timestamp(self):
         if timestamp := self.json.get('taken_at_timestamp', self.json.get('taken_at', '')):
             return datetime.utcfromtimestamp(timestamp)
         return ''
-
-    @property
-    def media_url(self):
-        return self.video_url if self.is_video else self.thumbnail_url
 
     @property
     def caption(self):
@@ -59,10 +53,17 @@ class InstaPost:
             return caption_edge[0].get('node', {}).get('text', '')
         return ''
 
-    def add_tweet_data(self, tweet: "tweepy.models.Status"):
-        self.tweet_data = {
-            'link': tweet.entities['urls'][0]['url'],
-            'created_at': str(tweet.created_at),
-            'text': tweet.text
-        }
-        return True
+    @property
+    def media_url(self):
+        return self.video_url if self.is_video else self.thumbnail_url
+
+    @property
+    def thumbnail_url(self):
+        return self.json.get('display_url',
+                             self.json.get('thumbnail_src',
+                                           self.json.get('thumbnail_resources',
+                                                         [{}])[-1].get('src', '')))
+
+    @property
+    def is_carousel(self):
+        return self.json.get('media_type') == 8
--- InstaTweet/instauser.py
+++ InstaTweet/instauser.py
@@ -1,19 +1,19 @@
 from . import InstaPost
 
 
-class InstaUser(object):
-    """Convenience wrapper for Instagram profile API response"""
+class InstaUser:
+    """Minimalistic API response wrapper for an Instagram profile"""
 
-    def __init__(self, user_json):
-        self.json = user_json
+    def __init__(self, data: dict):
+        """Initialize an :class:`InstaUser`
+
+        :param data: the API response JSON to use as source data
+        """
+        self.json = data
 
     @property
     def user_data(self):
         return self.json.get('graphql', {}).get('user')
-
-    @property
-    def id(self):
-        return int(self.user_data.get('id'))
 
     @property
     def media_data(self):
@@ -22,3 +22,7 @@ class InstaUser(object):
     @property
     def posts(self):
         return [InstaPost(media['node']) for media in self.media_data['edges']]
+
+    @property
+    def id(self):
+        return int(self.user_data.get('id', -1))
