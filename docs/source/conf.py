524aaaa1c48e36329c9a7cb5b33ae09a87b63293
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-07-19 04:42:56 -0400

    Update conf.py
    for why was that even there question mark ?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.restbuilder'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
