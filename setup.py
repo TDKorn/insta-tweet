e834df9d1ead76dce40fece532ff6542aac3cf77
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-09-02 22:43:38 -0400

    BUMP VERSION TO ```2.0.0```
    OHHHHHHHHH YEAAAAAA DONT' MIND IF I DO

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_pypi_desc(rst_file=LONG_DESCRIPTION_SRC):
 setup(
     name='insta-tweet',
     packages=['InstaTweet'],
-    version='2.0.0b36',
+    version='2.0.0',
     license='MIT',
     description='Automatically Repost Content From Instagram to Twitter',
     long_description=get_pypi_desc(LONG_DESCRIPTION_SRC),
