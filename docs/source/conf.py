130b27df8675cef5019513e2f0cb9e5a7fe81216
TDKorn <96394652+TDKorn@users.noreply.github.com>
2023-04-16 03:47:58 -0400

    Add ```sphinx-github-style```
    I literally made a whole package for this then proceeded to not implement it in any of my repos 💗

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,25 +13,20 @@
 import os
 import re
 import sys
-import shutil
-import inspect
-import subprocess
 import pkg_resources
-
+import sphinx_github_style
 
 # ============================== Build Environment ==============================
 # Build behaviour is dependent on environment
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
-# If building locally, configure path so I don't need to run setup.py install first
 if not on_rtd:
     sys.path.insert(0, os.path.abspath('../../'))
 
 sys.path.append(os.path.abspath('.'))
-# Add custom Pygments style if HTML
-if 'html' in sys.argv:
-    pygments_style = 'tdk_style.TDKStyle'
-else:
+
+# Use default Pygments style if not HTML
+if 'html' not in sys.argv:
     pygments_style = 'sphinx'
 
 # on_rtd = True  # Uncomment for testing RTD builds locally
@@ -42,10 +37,11 @@ else:
 project = 'InstaTweet'
 copyright = '2022, Adam Korn'
 author = 'Adam Korn'
+repo = "insta-tweet"
 
-# The full version, including alpha/beta/rc tags
-# Simplify things by using the version from setup.py
-version = pkg_resources.require("insta-tweet")[0].version
+# Simplify things by using the installed version
+pkg = pkg_resources.require(repo)[0]
+version = pkg.version
 release = version
 
 
@@ -91,7 +87,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.viewcode',
-    '_ext.linkcode',
+    'sphinx_github_style',
 ]
 
 
@@ -164,118 +160,34 @@ html_theme_options = {
     'prev_next_buttons_location': 'both',
 }
 
-
-# ============================ Linkcode Extension Settings ============================
-#
-# Keeping linkcode separate from other extension settings since it's only for RTD builds (and long af)
-# To be clear: if on_rtd == False, this whole section is skipped and only viewcode is used
-#
-#
-#                  Adapted from https://github.com/nlgranger/SeqTools (ily)
-#
-#
-
-# Get the blob to link to on GitHub
-linkcode_revision = "master"
-
-try:
-    # lock to commit number
-    cmd = "git log -n1 --pretty=%H"
-    head = subprocess.check_output(cmd.split()).strip().decode('utf-8')
-    linkcode_revision = head
-
-    # if we are on master's HEAD, use master as reference
-    cmd = "git log --first-parent master -n1 --pretty=%H"
-    master = subprocess.check_output(cmd.split()).strip().decode('utf-8')
-    if head == master:
-        linkcode_revision = "master"
-
-    # if we have a tag, use tag as reference
-    cmd = "git describe --exact-match --tags " + head
-    tag = subprocess.check_output(cmd.split(" ")).strip().decode('utf-8')
-    linkcode_revision = tag
-
-except subprocess.CalledProcessError:
-    pass
-
-
-# Add the "Edit on GitHub" link at the top
+# Set the "Edit on GitHub" link to use the current commit
 html_context = {
     'display_github': True,
     'github_user': 'TDKorn',
-    'github_repo': 'insta-tweet',
-    'github_version': f'{linkcode_revision}/docs/source/'
+    'github_repo': repo,
 }
 
-# Source URL template; formatted + returned by linkcode_resolve
-linkcode_url = "https://github.com/tdkorn/insta-tweet/blob/" \
-               + linkcode_revision + "/{filepath}#L{linestart}-L{linestop}"
+# ~~~~ Sphinx GitHub Style ~~~~
+#
+# Top level package name
+top_level = pkg.get_metadata("top_level.txt").strip()
 
-# Hardcoded Top Level Module Path // since InstaTweet isn't PyPi release name :(  it could be tho...
-modpath = pkg_resources.require('insta-tweet')[0].location
+# Blob to use when linking to GitHub source code
+linkcode_blob = 'last_tag'
+
+# Text to use for the linkcode link
+linkcode_link_text = "View on GitHub"
+
+# Source URL template; formatted + returned by linkcode_resolve
+linkcode_url = sphinx_github_style.get_linkcode_url(linkcode_blob, html_context)
+
+
+# Use default linkcode_resolve function to generate links for rst ref replacements
+linkcode_func = sphinx_github_style.get_linkcode_resolve(linkcode_url)
 
 
 def linkcode_resolve(domain, info):
-    """Returns a link to the source code on GitHub, with appropriate lines highlighted
-
-    Adapted from https://github.com/nlgranger (ily)
-    """
-    if domain != 'py' or not info['module']:
-        return None
-
-    modname = info['module']
-    fullname = info['fullname']
-
-    submod = sys.modules.get(modname)
-    if submod is None:
-        print(f'No submodule found for {fullname}')
-        return None
-
-    obj = submod
-    for part in fullname.split('.'):
-        try:
-            obj = getattr(obj, part)
-            print(obj)
-        except Exception:
-            print(f'error getting part? obj = {obj}, part = {part})')
-            return None
-
-    try:
-        filepath = os.path.relpath(inspect.getsourcefile(obj), modpath)
-        if filepath is None:
-            print(f'No filepath found for {obj} in module {modpath}...?')
-            return
-    except Exception as e:
-        return print(  # ie. None
-            f'Exception raised while trying to retrieve module path for {obj}:',
-            e, sep='\n'
-        )
-
-    try:
-        source, lineno = inspect.getsourcelines(obj)
-    except OSError:
-        print(f'failed to get source lines for {obj}')
-        return None
-    else:
-        linestart, linestop = lineno, lineno + len(source) - 1
-
-    # Format link using the filepath of the source file plus the line numbers
-    if on_rtd:
-        # Ex. InstaTweet\instatweet.py --> InstaTweet/instatweet.py
-        filepath = '/'.join(filepath.split('\\'))
-
-    else:
-        # For local builds, I must be doing something wrong because the
-        # paths are always like "..\..\..\InstaTweet\module.py"
-        filepath = '/'.join(filepath.lstrip('..\\').split('\\'))
-
-    # Example of final link: https://github.com/tdkorn/insta-tweet/blob/docs/InstaTweet/instatweet.py#L73-L117
-    final_link = linkcode_url.format(
-        filepath=filepath,
-        linestart=linestart,
-        linestop=linestop
-    )
-    print(f"Final Link for {fullname}: {final_link}")
+    final_link = linkcode_func(domain, info)
     replace_autodoc_refs_with_linkcode(
         info=info,
         link=final_link,
